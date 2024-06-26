3927fee96a4d6a9f83948703687a2401eb9976c1
laino-dahwa <79699605+laino-dahwa@users.noreply.github.com>
2024-06-26 12:33:37 +0200

    Initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,10 @@
+
+const Page = () => {
+    return (
+      <div>
+         <h1>This is the customers page </h1>
+      </div>
+    )
+  }
+  
+  export default Page
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+
+
+const Page = () => {
+  return (
+    <div>
+       <h1>This is the Dashboard page </h1>
+    </div>
+  )
+}
+
+export default Page
